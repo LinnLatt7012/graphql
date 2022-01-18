5c261f9579559a428394e784e7c6b94d41a83c0a
plinnlatt <plinnlatt70@gmail.com>
2022-01-18 08:06:11 +0630

    first-half of e-commerce-app
    -returning product array
    -returning specific data from products array

--- e-commerce_app/index.js
+++ e-commerce_app/index.js
@@ -1,1 +1,44 @@
-const { } = require("apollo-server");
+const { ApolloServer, gql} = require("apollo-server");
+const { products } = require("./intialdata")
+
+const typeDefs = gql`
+    type Query{
+        hello: [String]
+        products:[Product!]!
+        product(id: ID!):Product
+    },
+    type Product {
+        id:ID
+        name:String!
+        description:String!
+        quantity: Int!
+        price: Float!
+        onSale: Boolean!
+    }
+`
+const resolvers = {
+    Query:{
+        hello: ()=>{
+            return ["Wold!","Hi","Fine"]
+        },
+        products:()=>{
+            return products
+        },
+        product:(parents, args,context)=>{
+            const productId = args.id;
+
+            const product= products.find(product => product.id===productId);
+            return product;
+        }
+
+    },
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+}
+);
+server.listen().then(({url})=>{
+    console.log("Server is ready at "+ url);
+})
